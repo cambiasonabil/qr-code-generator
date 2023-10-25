b684fb3a64cb8ad5716c90ab03bfd1ce9f27a6d1
nabilcambiaso <nabil.cambiaso@gmail.com>
2023-10-25 21:32:11 +0100

    Ready qrCode

--- pages/components/QrCodeGenerator.js
+++ pages/components/QrCodeGenerator.js
@@ -1,50 +1,67 @@
 import React, { useState } from 'react';
-import QRCode from 'qrcode.react';
-import axios from 'axios';
-
+// import QRCode from 'qrcode.react';
+import { QRCode } from 'react-qrcode-logo';
 function QrCodeGenerator() {
-  const [text, setText] = useState('');
-  const [size, setSize] = useState('16');
-  const [faviconUrl, setFaviconUrl] = useState('');
+  const [url, setUrl] = useState('');
+  const [qrCodeUrl, setQrCodeUrl] = useState('');
 
-  const fetchFavicon = async () => {
-    try {
-      // Construct the URL for fetching the favicon based on the input URL and selected size
-      const faviconApiUrl = `https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${text}&size=${size}`;
+  // Function to handle generating the QR code
+  const generateQRCode = () => {
+    let formattedUrl = url.trim();
 
-      // Make a request to fetch the favicon
-      const response = await axios.get(faviconApiUrl);
-console.log(response)
-      // Check if the response contains data
-      if (response.data) {
-        setFaviconUrl(faviconApiUrl); // Set the favicon URL
-      } else {
-        console.error('No favicon found');
-      }
-    } catch (error) {
-      console.error('Error fetching favicon:', error);
+    // Check if the URL starts with "http://" or "https://"
+    if (!formattedUrl.startsWith('http://') && !formattedUrl.startsWith('https://')) {
+      formattedUrl = 'https://' + formattedUrl;
     }
+
+    setQrCodeUrl(formattedUrl);
   };
 
   return (
     <div>
+      <h2>QR Code Generator</h2>
       <input
         type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Enter website URL"
+        placeholder="Enter website URL (without http:// or https://)"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
       />
-      <select value={size} onChange={(e) => setSize(e.target.value)}>
-        <option value="16">16x16</option>
-        <option value="32">32x32</option>
-        <option value="64">64x64</option>
-      </select>
-      <button onClick={fetchFavicon}>Fetch Favicon</button>
-      {faviconUrl && (
-        <QRCode value={text}>
-          {/* Display the fetched favicon as an image in the center of the QR code */}
-          <image x={25} y={25} width={size} height={size} xlinkHref={faviconUrl} />
-        </QRCode>
+      <button onClick={generateQRCode}>Generate QR Code</button>
+
+      {qrCodeUrl && (
+        <div>
+          <p>Website URL: {qrCodeUrl}</p>
+          {/* <QRCode value={qrCodeUrl}
+          imageSettings={{
+            src:`https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://mediaelegant.com&size=16`,
+            x: 32,
+            y: 32,
+            height: 20,
+            width: 20,
+            excavate: true
+
+          }}
+           /> */}
+           <QRCode value={qrCodeUrl}
+           logoImage={`https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${qrCodeUrl}&size=128`}
+           removeQrCodeBehindLogo={true}
+           size={350}
+           ecLevel={'H'}
+           eyeColor={'grey'}
+           eyeRadius={[[10, 10, 0, 10],50,5]}
+          //  logoHeight={32}
+          //  logoWidth={32}
+          //  bgColor='black'
+          //  fgColor='white'
+          //  logoPadding={10}
+          logoHeight={64} // Adjust logo height to your preference
+          logoWidth={64}  // Adjust logo width to your preference
+          bgColor='white'  // Use a light background color to improve readability
+          fgColor='black'  // Use a dark foreground color to improve readability
+          logoPadding={5}  // Adjust logo padding to your preference
+          logoPaddingStyle={'circle'}
+            />
+        </div>
       )}
     </div>
   );
