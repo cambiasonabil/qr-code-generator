1137a5505d79ee9b28d3a341b5a2907629384a55
nabilcambiaso <nabil.cambiaso@gmail.com>
2023-10-30 02:10:23 +0100

    Added ads

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import QrCodeGenerator from './components/QrCodeGenerator'
+
+
 
 export default function Home() {
   return (
-    <QrCodeGenerator/>
-  )
+    <div>
+      <Head>
+        <title>My Next.js App</title>
+        <meta name="description" content="Generate qr code" />
+        <link rel="icon" href="/favicon.ico" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3431156667078669"
+          crossorigin="anonymous"></script>
+        <meta name="google-adsense-account" content="ca-pub-3431156667078669" />
+      </Head>
+
+      <main>
+        <h1>Welcome to My Service</h1>
+        <QrCodeGenerator />
+      </main>
+
+      <footer>
+        No copyright
+      </footer>
+    </div>
+  );
 }
